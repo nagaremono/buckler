ca8af7cefc14a8f8c4a719955281e5071fc98e84
nagaremono <ediguruhdev@gmail.com>
2025-04-28 23:38:09 +0800

    feat: introduce router type

--- app/main.go
+++ app/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -16,6 +15,8 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+
+	initRouter()
 
 	for {
 		conn, err := l.Accept()
@@ -28,6 +29,15 @@ func main() {
 }
 
 var dirFlag = flag.String("directory", "", "")
+
+var router *Router
+
+func initRouter() {
+	router = NewRouter()
+	router.register("GET", "/echo/:s", handleEcho)
+	router.register("GET", "/user-agent", handleUserAgent)
+	router.register("GET", "/files/:file", handleFiles)
+}
 
 func handle(c net.Conn) {
 	req, err := parseRequest(c)
@@ -49,66 +59,9 @@ func handle(c net.Conn) {
 			statusText: "OK",
 			protocol:   "HTTP/1.1",
 		}
-	} else if strings.HasPrefix(req.target, "/echo/") {
-		handleEcho(c, req)
-		return
-	} else if strings.HasPrefix(req.target, "/user-agent") {
-		handleUserAgent(c, req)
-		return
-	} else if strings.HasPrefix(req.target, "/files") {
-		handleFiles(c, req)
-		return
-	} else {
-		res = &Response{
-			status:     404,
-			statusText: "Not Found",
-			protocol:   "HTTP/1.1",
-		}
-	}
-
-	_, err = c.Write(res.Bytes())
-	if err != nil {
-		fmt.Println(err)
-		res := &Response{
-			status:     500,
-			statusText: "Internal Server Error",
-			protocol:   "HTTP/1.1",
-		}
 		c.Write(res.Bytes())
-	}
-}
-
-func parseRequest(c net.Conn) (*Request, error) {
-	data := make([]byte, 512)
-	reqString := ""
-
-	for {
-		read, err := c.Read(data)
-		if err != nil {
-			return nil, err
-		}
-		reqString += string(data)
-		if read < 512 {
-			break
-		}
+		return
 	}
 
-	requestLineStr, headerAndBody, _ := strings.Cut(reqString, "\r\n")
-	requestLine := strings.Split(requestLineStr, " ")
-
-	headerStr, body, _ := strings.Cut(headerAndBody, "\r\n\r\n")
-	headers := map[string]string{}
-
-	for _, v := range strings.Split(headerStr, "\r\n") {
-		header := strings.Split(v, ": ")
-		headers[header[0]] = header[1]
-	}
-
-	return &Request{
-		method:   requestLine[0],
-		target:   requestLine[1],
-		protocol: requestLine[2],
-		headers:  headers,
-		body:     []byte(body),
-	}, nil
+	router.match(req, c)
 }
--- app/types.go
+++ app/types.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-type Handler func(c net.Conn)
+type Handler func(c net.Conn, r *Request)
 
 type Request struct {
 	method   string
@@ -14,6 +14,41 @@ type Request struct {
 	protocol string
 	headers  map[string]string
 	body     []byte
+}
+
+func parseRequest(c net.Conn) (*Request, error) {
+	data := make([]byte, 512)
+	reqString := ""
+
+	for {
+		read, err := c.Read(data)
+		if err != nil {
+			return nil, err
+		}
+		reqString += string(data)
+		if read < 512 {
+			break
+		}
+	}
+
+	requestLineStr, headerAndBody, _ := strings.Cut(reqString, "\r\n")
+	requestLine := strings.Split(requestLineStr, " ")
+
+	headerStr, body, _ := strings.Cut(headerAndBody, "\r\n\r\n")
+	headers := map[string]string{}
+
+	for _, v := range strings.Split(headerStr, "\r\n") {
+		header := strings.Split(v, ": ")
+		headers[header[0]] = header[1]
+	}
+
+	return &Request{
+		method:   requestLine[0],
+		target:   requestLine[1],
+		protocol: requestLine[2],
+		headers:  headers,
+		body:     []byte(body),
+	}, nil
 }
 
 type Response struct {
@@ -31,4 +66,76 @@ func (r *Response) String() string {
 
 func (r *Response) Bytes() []byte {
 	return []byte(r.String())
+}
+
+type RouteHandler struct {
+	target  string
+	method  string
+	handler Handler
+}
+
+type Router struct {
+	rHandlers []*RouteHandler
+}
+
+func NewRouter() *Router {
+	return &Router{
+		rHandlers: []*RouteHandler{},
+	}
+}
+
+func (r *Router) register(method string, target string, handler Handler) {
+	r.rHandlers = append(r.rHandlers, &RouteHandler{
+		target,
+		method,
+		handler,
+	})
+}
+
+func (r *Router) match(req *Request, c net.Conn) {
+	var handler Handler
+	for _, rh := range r.rHandlers {
+		if matcher(req, rh) {
+			handler = rh.handler
+		}
+	}
+
+	if handler == nil {
+		res := &Response{
+			status:     404,
+			statusText: "Not Found",
+			protocol:   "HTTP/1.1",
+		}
+
+		_, err := c.Write(res.Bytes())
+		if err != nil {
+			fmt.Println(err)
+			res := &Response{
+				status:     500,
+				statusText: "Internal Server Error",
+				protocol:   "HTTP/1.1",
+			}
+			c.Write(res.Bytes())
+		}
+	}
+
+	handler(c, req)
+}
+
+func matcher(req *Request, rh *RouteHandler) bool {
+	if req.method != rh.method {
+		return false
+	}
+	targetParts := strings.Split(req.target, "/")
+	handlerParts := strings.Split(rh.target, "/")
+	for i := 0; i < len(targetParts); i++ {
+		if i >= len(handlerParts) {
+			return false
+		}
+		if !strings.HasPrefix(handlerParts[i], ":") && targetParts[i] != handlerParts[i] {
+			return false
+		}
+	}
+
+	return true
 }
